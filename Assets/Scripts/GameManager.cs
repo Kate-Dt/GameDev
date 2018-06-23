cfd9c08c7efbe39678f5d6d92614b1684bc94fb6
Kate-Dt <dotsiuk.k@gmail.com>
2018-06-23 04:30:04 +0200

    initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    bool gameEnded = false;
+
+    public float restartDelay = 2f;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+	public void EndGame()
+    {
+        if (gameEnded == false)
+        {
+            gameEnded = true;
+            Invoke("Restart", restartDelay);
+        }
+
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+    public Rigidbody rigidbody;
+    public float forwardForce = 2000f;
+    public float sidewayForce = 500f;
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rigidbody.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rigidbody.AddForce(sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rigidbody.AddForce(-sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rigidbody.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
