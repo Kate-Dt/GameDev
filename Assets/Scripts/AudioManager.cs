b70c261012eae7aac4d6a44b9e8e440f13aa829d
DashaTyshch <dasha.tyshenko98@gmail.com>
2018-06-26 04:56:09 +0300

    sound on/off

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,10 +2,21 @@
 using UnityEngine.Audio;
 using System;
 using System.Linq;
+using UnityEngine.UI;
+
 public class AudioManager: MonoBehaviour {
 	public Sound[] sounds;
+	public Text soundT = null; 
+	bool isPlayed = true;
+
 	// Use this for initialization
 	void Start () {
+		if (isPlayed) {
+			foreach (var src in FindObjectsOfType <AudioSource>()) 
+			{
+				src.Play();
+			}
+		}
 		foreach (Sound s in sounds) {
 			s.source = gameObject.AddComponent<AudioSource> ();
 			s.source.clip = s.clip;
@@ -13,12 +24,41 @@ public class AudioManager: MonoBehaviour {
 			s.source.pitch = s.pitch;
 		}
 	}
+
+	void Awake() {
+		isPlayed = PlayerPrefs.GetInt ("sound", 1) == 1;
+		if(!isPlayed)
+			soundT.text = "Music OFF";
+	}
+
 	
 	public void Play (string name)
 	{
+		if (!isPlayed)
+			return;
 		Sound s = sounds.FirstOrDefault(sound => sound.name == name);
 		if (s == null)
 			return;
 		s.source.Play ();
+	}
+
+	public void Sound() {
+		if (isPlayed) {
+			soundT.text = "Music OFF";
+			foreach (var src in FindObjectsOfType <AudioSource>()) {
+				src.mute = true;
+			}
+			PlayerPrefs.SetInt ("sound", 0);
+			isPlayed = false;
+		} else {
+			soundT.text = "Music ON";
+			foreach (var src in FindObjectsOfType <AudioSource>()) {
+				src.mute = false;
+				src.Play ();
+			}
+			PlayerPrefs.SetInt ("sound", 1);
+			isPlayed = true;
+		}
+
 	}
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -29,4 +29,8 @@ public class GameManager : MonoBehaviour {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
+	public void OpenMenu() {
+		Time.timeScale = 1f;
+		SceneManager.LoadScene("StartMenu");
+	}
 }
