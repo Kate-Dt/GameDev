49311272596e9cf9839c4e9ccc46c9f688c9f89a
DashaTyshch <dasha.tyshenko98@gmail.com>
2018-06-26 03:41:31 +0300

    add info panel

---
+++ Assets/Scripts/InfoPanel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoPanel : MonoBehaviour {
+
+	public static bool isInfo = false;
+	public GameObject Panel;
+
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.F1)) {
+			if (isInfo)
+				Show ();
+			else
+				Hide ();
+		}
+	}
+	public void Show() {
+		Panel.SetActive (true);
+		isInfo = true;
+	}
+	public void Hide() {
+		Panel.SetActive (false);
+		isInfo = false;
+	}
+}
